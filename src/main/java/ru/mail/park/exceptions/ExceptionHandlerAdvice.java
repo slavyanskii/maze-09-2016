67d8cdcacec339ad3f07236d825efe54151fb84e
slavyanskii <mossbka@gmail.com>
2016-10-06 14:14:48 +0300

    ErrorResponse in Handler

--- src/main/java/ru/mail/park/exceptions/ExceptionHandlerAdvice.java
+++ src/main/java/ru/mail/park/exceptions/ExceptionHandlerAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.mail.park.Application;
+import ru.mail.park.main.RegistrationController;
 
 /**
  * Created by viacheslav on 03.10.16.
@@ -16,6 +17,32 @@ public class ExceptionHandlerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity handleGlobalException(Exception e) {
         Application.logger.error(e);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{Server error}");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), "Server error"));
+    }
+
+    private static final class ErrorResponse {
+        private String msg;
+        private String error;
+
+        private ErrorResponse(String error, String msg) {
+            this.msg = msg;
+            this.error = error;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
+        }
+
+        public String getError() {
+            return error;
+        }
+
+        public void setError(String error) {
+            this.error = error;
+        }
     }
 }
